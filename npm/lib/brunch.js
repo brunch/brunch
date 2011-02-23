b3640432f82de4aa973c746fa180ed2b9823fbf5
Thomas Schranz <tosh@deck.cc>
2011-02-23 13:11:10 +0100

    updated stylus dependency

--- npm/lib/brunch.js
+++ npm/lib/brunch.js
@@ -8,7 +8,7 @@
   glob = require('glob');
   brunch = require('brunch');
   helpers = require('./helpers');
-  exports.VERSION = '0.3.6';
+  exports.VERSION = '0.3.7';
   exports["new"] = function(projectName, options, callback) {
     var projectTemplatePath;
     exports.options = options;
