27bc357c4e46fd102e06493a4025838a42c7bb66
Thomas Schranz <tosh@deck.cc>
2011-02-25 11:12:51 +0100

    bumped stylus version

--- npm/lib/brunch.js
+++ npm/lib/brunch.js
@@ -8,7 +8,7 @@
   glob = require('glob');
   brunch = require('brunch');
   helpers = require('./helpers');
-  exports.VERSION = '0.4.2';
+  exports.VERSION = '0.4.3';
   exports["new"] = function(projectName, options, callback) {
     var projectTemplatePath;
     exports.options = options;
--- npm/src/brunch.coffee
+++ npm/src/brunch.coffee
@@ -11,7 +11,7 @@ brunch    = require 'brunch'
 helpers   = require './helpers'
 
 # the current brunch version number
-exports.VERSION = '0.4.2'
+exports.VERSION = '0.4.3'
 
 # project skeleton generator
 exports.new = (projectName, options, callback) ->
