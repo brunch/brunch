b96b305e9b1090ca07b6ac0de4f3ecba018f1b42
Nikolaus Graf <nik@deck.cc>
2011-03-14 13:38:16 +0100

    fixed express server

--- npm/template/express/server/main.js
+++ npm/template/express/server/main.js
@@ -6,7 +6,7 @@ var app = express.createServer();
 
 app.configure(function(){
     app.set('views', "brunch/build");
-    app.use(express.staticProvider("brunch/build"));
+    app.use(express.static("brunch/build"));
 });
 
 app.get('/', function(req, res){
