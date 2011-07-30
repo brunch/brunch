8ab06379213bab761ab1c9cab45c04ac4dcccda5
Nikolaus Graf <nik@deck.cc>
2011-07-30 17:33:34 +0200

    fixed tests

--- test/server/server.js
+++ test/server/server.js
@@ -1,19 +1,21 @@
 var util = require('util');
 var path = require('path');
 var port = process.argv[2];
+var brunchPath = process.argv[3];
 
 var express = require("express");
 var app = express.createServer();
 
-var buildPath = path.join(process.argv[3], 'build');
-
 app.configure(function(){
-    app.set('views', buildPath);
-    app.use(express.static(buildPath));
+    app.use(express.static(brunchPath));
+    // setup views to render index.html
+    app.set('views', brunchPath);
+    app.set('view options', { layout: false });
+    app.register('.html', require('eco'));
 });
 
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('index.html');
 });
 
 app.listen(parseInt(port, 10));
