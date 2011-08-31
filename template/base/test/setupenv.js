8fe98325080afe6e5b44ab090ece608d6d98933e
Omar Khan <omarkhan@gmx.com>
2011-08-31 18:29:32 +0100

    Upgrade to Backbone 0.5.3 in setupenv.js

--- template/base/test/setupenv.js
+++ template/base/test/setupenv.js
@@ -1,7 +1,7 @@
 // Inject _ and Backbone globals to simulate browser environment.
 
 global._ = require(__dirname + '/../src/vendor/underscore-1.1.7');
-global.Backbone = require(__dirname + '/../src/vendor/backbone-0.5.2');
+global.Backbone = require(__dirname + '/../src/vendor/backbone-0.5.3');
 
 
 // Uncomment the following line to include jquery. Note that this requires the
