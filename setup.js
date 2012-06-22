60aa582fba992d18a2b2bcc09accb9bcdb32eef4
Paul Miller <paul@paulmillr.com>
2012-06-22 13:17:03 +0300

    Use setup.js postinstall only if `lib/` ain't exists.

--- setup.js
+++ setup.js
@@ -1,7 +1,10 @@
 var exec = require('child_process').exec;
 var sysPath = require('path');
+var fs = require('fs');
 
 var mode = process.argv[2];
+
+var fsExists = fs.exists || sysPath.exists;
 
 var execute = function(pathParts, params, callback) {
   if (callback == null) callback = function() {};
@@ -15,7 +18,10 @@ var execute = function(pathParts, params, callback) {
 };
 
 if (mode === 'postinstall') {
-  execute(['node_modules', 'coffee-script', 'bin', 'coffee'], '-o lib/ src/');
+  fsExists(sysPath.join(__dirname, 'lib'), function(exists) {
+    if (exists) return;
+    execute(['node_modules', 'coffee-script', 'bin', 'coffee'], '-o lib/ src/');
+  });
 } else if (mode === 'test') {
   execute(['node_modules', 'mocha', 'bin', 'mocha'],
     '--compilers coffee:coffee-script --require test/common.coffee --colors');
