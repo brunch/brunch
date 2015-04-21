brunch/brunch

commit fd3c169209311a036e353bc99892f841a8bbdf0f

Refactor.

src/index.coffee
@@ -2,11 +2,12 @@
 
 initSkeleton = require 'init-skeleton'
 loggy = require 'loggy'
+watch = require './watch'
 
 start = ->
   isDebug = arguments[1]?.debug or arguments[2]?.debug
   process.env.DEBUG = 'brunch:*' if isDebug
-  (require './watch') arguments...
+  watch arguments...
 
 module.exports =
   new: (skeleton, path) ->

src/cli.coffee
@@ -38,12 +38,10 @@ watchCmd = program
   .action(commands.watch)
 
 addDeprecatedOpts = ->
-  add = (cmd) ->
+  [buildCmd, watchCmd].forEach (cmd) ->
     cmd
       .option('-c, --config [path]', '[DEPRECATED] path to config files')
       .option('-o, --optimize', '[DEPRECATED] same as `--env production`')
-  add buildCmd
-  add watchCmd
 
 # The function would be executed every time user run `bin/brunch`.
 exports.run = ->

src/fs_utils/asset.coffee
@@ -26,10 +26,10 @@ getAssetDirectory = (path, convention) ->
 
 # A static file that shall be copied to public directory.
 module.exports = class Asset
-  constructor: (@path, config) ->
-    directory = getAssetDirectory @path, config._normalized.conventions.assets
+  constructor: (@path, publicPath, assetsConvention) ->
+    directory = getAssetDirectory @path, assetsConvention
     @relativePath = sysPath.relative directory, @path
-    @destinationPath = sysPath.join config.paths.public, @relativePath
+    @destinationPath = sysPath.join publicPath, @relativePath
     debug "Initializing fs_utils.Asset %s", JSON.stringify {
       @path, directory, @relativePath, @destinationPath
     }

src/fs_utils/file_list.coffee
@@ -2,11 +2,11 @@
 
 debug = require('debug')('brunch:file-list')
 {EventEmitter} = require 'events'
+{normalize} = require 'path'
+fcache = require 'fcache'
 Asset = require './asset'
 SourceFile = require './source_file'
 {formatError} = require '../helpers'
-fcache = require 'fcache'
-sysPath = require 'path'
 
 startsWith = (string, substring) ->
   string.lastIndexOf(substring, 0) is 0
@@ -48,7 +48,7 @@ module.exports = class FileList extends EventEmitter
       when '[object Function]'
         test path
       when '[object String]'
-        startsWith sysPath.normalize(path), sysPath.normalize(test)
+        startsWith normalize(path), normalize(test)
       when '[object Array]'
         test.some((subTest) => @isIgnored path, subTest)
       else
@@ -126,7 +126,8 @@ module.exports = class FileList extends EventEmitter
     file
 
   _addAsset: (path) ->
-    file = new Asset path, @config
+    constructor: (@path, config) ->
+    file = new Asset path, config.paths.public, config._normalized.conventions.assets
     @assets.push file
     file
 

src/fs_utils/generate.coffee
@@ -1,11 +1,10 @@
 'use strict'
 
 debug = require('debug')('brunch:generate')
-fs = require 'fs'
-sysPath = require 'path'
+{basename} = require 'path'
 waterfall = require 'async-waterfall'
 anysort = require 'anysort'
-common = require './common'
+{writeFile} = require './common'
 {SourceMapConsumer, SourceMapGenerator, SourceNode} = require 'source-map'
 
 # Sorts by pattern.
@@ -125,7 +124,7 @@ generate = (path, sourceFiles, config, optimizers, callback) ->
   joinToValue = config.files["#{type}s"].joinTo[joinKey]
   sorted = sort sourceFiles, config, joinToValue
 
-  {code, map} = concat sorted, path, type, config._normalized.modules.definition, config._normalized.packageInfo['component'].aliases
+  {code, map} = concat sorted, path, type, config._normalized.modules.definition, config._normalized.packageInfo.component.aliases
 
   withMaps = (map and config.sourceMaps)
   mapPath = "#{path}.map"
@@ -139,7 +138,7 @@ generate = (path, sourceFiles, config, optimizers, callback) ->
           .replace config.paths.public, ''
           .replace '\\', '/'
       else
-        sysPath.basename mapPath
+        basename mapPath
       controlChar = if config.sourceMaps is 'old' then '@' else '#'
       data.code += if type is 'javascript'
         "\n//#{controlChar} sourceMappingURL=#{mapRoute}"

commit 6143352f8573ac23865af83a617d9dc1101c1804

Fix --debug CLI switch
Reverts the change in fd3c169209311a036e353bc99892f841a8bbdf0f which broke this feature

src/index.coffee
@@ -2,12 +2,11 @@
 
 initSkeleton = require 'init-skeleton'
 loggy = require 'loggy'
-watch = require './watch'
 
 start = ->
   isDebug = arguments[1]?.debug or arguments[2]?.debug
   process.env.DEBUG = 'brunch:*' if isDebug
-  watch arguments...
+  (require './watch') arguments...
 
 module.exports =
   new: (skeleton, path) ->