brunch/brunch

commit b4dcb83964fbef5fcba7cb9274df37c522f45633

Refactor. Move out two packages.

lib/deppack/index.js
@@ -2,10 +2,10 @@
 
 const sysPath = require('path');
 const fs = require('fs');
-const mediator = require('./mediator');
-const modules = require('./deppack/modules');
-const explore = require('./deppack/explore');
-const helpers = require('./deppack/helpers');
+const mediator = require('../mediator');
+const modules = require('./modules');
+const explore = require('./explore');
+const helpers = require('./helpers');
 
 const dir = sysPath.dirname(sysPath.dirname(require.resolve('.')));
 const readFile = (path, callback) => fs.readFile(path, {encoding: 'utf8'}, callback);
@@ -36,9 +36,9 @@ const needsProcessing = file => file.path.indexOf('node_modules') !== -1 &&
 
 exports.exploreDeps = explore;
 exports.wrapInModule = wrapInModule;
-exports.loadInit = require('./deppack/init');
+exports.loadInit = require('./init');
 exports.needsProcessing = needsProcessing;
-exports.processFiles = require('./deppack/process');
+exports.processFiles = require('./process');
 
 exports.isShim = path => path.indexOf(dir) === 0;
 exports.isNpm = path => {
@@ -52,3 +52,6 @@ exports.isNpm = path => {
 exports.isNpmJSON = path => {
   return exports.isNpm(path) && path.indexOf('.json') !== -1;
 };
+
+// exports.init = require('./init');
+// exports.process = require('./process');

lib/workers/index.js
@@ -1,5 +1,5 @@
 'use strict';
-const WorkerManager = require('./workers/manager');
+const WorkerManager = require('./manager');
 
 let manager;
 

bin/brunch
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // Not using ES6 in that file since we want it to "launch" on older nodes.
-global.brunchStartTime = Date.now();
+global.appStartTime = Date.now();
 
 var sysPath = require('path');
 var fs = require('fs');

lib/application.js
@@ -222,7 +222,7 @@ const setConfigDefaults = exports.setConfigDefaults = (config, configPath) => {
   if (paths.root == null) paths.root = '.';
   ensureType(paths, 'root', 'string');
 
-  if (paths['public'] == null) paths['public'] = joinRoot('public');
+  if (paths.public == null) paths.public = joinRoot('public');
   ensureType(paths, 'public', 'string');
 
   if (paths.watched == null) paths.watched = ['app', 'test', 'vendor'].map(joinRoot);
@@ -264,9 +264,11 @@ const setConfigDefaults = exports.setConfigDefaults = (config, configPath) => {
 
   // Server.
   const server = config.server != null ? config.server : config.server = {};
+  server.publicPath = paths.public;
   if (server.base == null) server.base = '';
   if (server.port == null) server.port = 3333;
   if (server.run == null) server.run = false;
+  if (!config.persistent) server.run = false;
 
   // Overrides.
   const overrides = config.overrides != null ? config.overrides : config.overrides = {};

lib/plugins.js
@@ -2,8 +2,7 @@
 const sysPath = require('path');
 const debug = require('debug')('brunch:plugins');
 const logger = require('loggy');
-
-const speed = require('./speed');
+const speed = require('since-app-start');
 
 const slice = [].slice;
 const brunchPluginPattern = 'brunch'; // To filter brunch plugins.

lib/server.js
@@ -1,92 +1,0 @@
-'use strict';
-const sysPath = require('path');
-const spawn = require('child_process').spawn;
-const debug = require('debug')('brunch:server');
-const logger = require('loggy');
-const pushserve = require('pushserve');
-
-const longCallbackTime = 4000; // ms.
-
-const startServer = (config, callback) => {
-  if (!callback) callback = Function.prototype;
-  const serverOpts = config.server || {};
-  const port = parseInt(serverOpts.port, 10);
-  const host = config.server.hostname || 'localhost';
-  const serverCommand = serverOpts.command;
-  const serverPath = serverOpts.path;
-  const publicPath = config.paths.public;
-  let customServerTimeout;
-  let server;
-
-  const serverCb = () => {
-    clearTimeout(customServerTimeout);
-    const isCustom = serverPath || serverCommand;
-    logger.info(isCustom ?
-      'custom server started, initializing watcher' :
-      `application started on http://${host}:${port}/`
-    );
-    callback(null, server);
-  };
-  if (serverPath) {
-    logger.info('starting custom server');
-    try {
-      server = require(sysPath.resolve(serverPath));
-    } catch (error) {
-      logger.error(`couldn't load server ${serverPath}: ${error}`);
-    }
-    const serverFn = (() => {
-      if (typeof server === 'function') {
-        return server;
-      } else if (server && typeof server.startServer === 'function') {
-        return server.startServer.bind(server);
-      } else {
-        throw new Error('Brunch server file needs to export server function');
-      }
-    })();
-    const opts = {port: port, hostname: host, path: publicPath};
-    const serverConfig = Object.assign(opts, serverOpts.config || {});
-    debug(`Invoking custom startServer with: ${JSON.stringify(serverConfig)}`);
-    customServerTimeout = setTimeout(() => {
-      logger.warn('custom server taking a long time to start');
-      return logger.warn('**don\'t forget to invoke callback()**');
-    }, longCallbackTime);
-    switch (serverFn.length) {
-      case 1:
-        return serverFn(serverCb);
-      case 2:
-        return serverFn(serverConfig, serverCb);
-      default:
-        return serverFn(port, publicPath, serverCb);
-    }
-  } else if (serverCommand) {
-    const commandComponents = serverCommand.split(' ');
-    debug(`Invoking custom server command with: ${serverCommand}`);
-    if (!commandComponents.length) {
-      throw new Error('Invalid custom server command');
-    }
-    /* fn to kill the custom server */
-    const child = spawn(commandComponents.shift(), commandComponents, {
-      stdio: 'inherit'
-    });
-    child.close = cb => {
-      child.kill();
-      return typeof cb === 'function' ? cb() : undefined;
-    };
-    setTimeout(serverCb, 0);
-    return child;
-  } else {
-    const opts = {noLog: true, path: publicPath};
-    return pushserve(Object.assign(opts, serverOpts), serverCb);
-  }
-};
-
-exports.init = (config) => {
-  if (!config.persistent || !config.server.run) return Promise.resolve(false);
-  return new Promise((resolve, reject) => {
-    let server;
-    server = startServer(config, error => {
-      if (error) return reject(error);
-      return resolve(server);
-    });
-  });
-};

lib/speed.js
@@ -1,17 +1,0 @@
-'use strict';
-
-const debug = require('debug')('brunch:speed');
-const startTime = 'brunchStartTime' in global ?
-  global.brunchStartTime :
-  Date.now();
-const profilings = {};
-
-exports.sinceStart = () => Date.now() - startTime;
-
-exports.addEntry = (name) => {
-  const ms = exports.sinceStart();
-  profilings[name] = ms;
-  return name + ' ' + ms + 'ms';
-};
-
-exports.profile = (name) => debug(exports.addEntry(name));

lib/watch.js
@@ -4,14 +4,14 @@ const fsaccess = require('fs').access;
 const logger = require('loggy');
 const chokidar = require('chokidar');
 const debug = require('debug')('brunch:watch');
+const speed = require('since-app-start');
+const serveBrunch = require('serve-brunch');
 
 const workers = require('./workers');
 const fsUtils = require('./fs_utils');
 const application = require('./application');
 const plugins = require('./plugins');
-const server = require('./server');
 const helpers = require('./helpers');
-const speed = require('./speed');
 
 const fsExists = (path) => {
   return new Promise(resolve => {
@@ -71,7 +71,7 @@ class BrunchWatcher {
         this.fileList = new fsUtils.FileList(cfg, this);
         return Promise.all([
           getWatchedPaths(cfg).then(setProp(this, '_watchedPaths')),
-          server.init(cfg).then(setProp(this, 'server')),
+          serveBrunch.serve(cfg.server).then(setProp(this, 'server')),
           plugins.init(cfg, onCompile).then(setProp(this, 'plugins'))
         ]);
       })

commit 773ac8eae6f86b6a4b872c6eaf5e3d1c8779c758

Fix deppack grouping for shims issue caused by b4dcb83

lib/deppack/index.js
@@ -7,7 +7,7 @@ const modules = require('./modules');
 const explore = require('./explore');
 const helpers = require('./helpers');
 
-const dir = sysPath.dirname(sysPath.dirname(require.resolve('.')));
+const dir = sysPath.dirname(sysPath.dirname(require.resolve('..')));
 const readFile = (path, callback) => fs.readFile(path, {encoding: 'utf8'}, callback);
 
 const wrapInModule = (filePath, opts) => new Promise((resolve, reject) => {