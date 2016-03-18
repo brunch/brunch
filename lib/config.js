brunch/brunch

commit 48cb5e04b3889d1ae2f939003ccb6eda0a1192cc

Switch to object-assign except for 1 place (needs investigation)

lib/config.js
@@ -7,6 +7,7 @@ const readComponents = require('read-components');
 const anymatch = require('anymatch');
 const coffee = require('coffee-script');
 const debug = require('debug')('brunch:config');
+const deepAssign = require('deep-assign');
 
 const loadInit = require('./deppack').loadInit;
 const mediator = require('./mediator');
@@ -14,7 +15,6 @@ const mdls = require('./modules');
 
 const _helpers = require('./helpers');
 const isWindows = exports.isWindows = _helpers.isWindows;
-const deepExtend = _helpers.deepExtend;
 const replaceSlashes = _helpers.replaceSlashes;
 
 coffee.register();
@@ -22,6 +22,20 @@ coffee.register();
 const defaultConfigFilename = 'brunch-config';
 const defaultServerFilename = 'brunch-server';
 
+const customDeepAssign = (object, properties) => {
+  Object.keys(properties).forEach(key => {
+    const value = properties[key];
+    if (toString.call(value) === '[object Object]') {
+      if (object[key] == null) object[key] = {};
+      customDeepAssign(object[key], value);
+    } else {
+      object[key] = value;
+    }
+  });
+  return object;
+};
+
+const specials = {on: 'off', off: 'on'};
 const applyOverrides = (config, options) => {
 
   /* Allow the environment to be set from environment variable */
@@ -40,7 +54,7 @@ const applyOverrides = (config, options) => {
         return;
       }
       config.overrides._default[prop] = {};
-      return deepExtend(config.overrides._default[prop], config[prop]);
+      deepAssign(config.overrides._default[prop], config[prop]);
     });
   }
   environments.forEach(override => {
@@ -48,7 +62,6 @@ const applyOverrides = (config, options) => {
     const overrideProps = (config.overrides && config.overrides[override]) || {};
 
     /* Special override handling for plugins.on|off arrays (gh-826) */
-    const specials = {on: 'off', off: 'on'};
     Object.keys(specials).forEach(k => {
       const v = specials[k];
       if (plug && plug[v]) {
@@ -61,7 +74,7 @@ const applyOverrides = (config, options) => {
         }));
       }
     });
-    return deepExtend(config, overrideProps, config.files);
+    customDeepAssign(config, overrideProps);
   });
   return config;
 };
@@ -515,7 +528,7 @@ exports.loadConfig = (persistent, opts, fromWorker) => {
     .then(fromWorker ? noop : addDefaultServer)
     .then(fromWorker ? noop : warnAboutConfigDeprecations)
     .then(config => applyOverrides(config, options))
-    .then(config => deepExtend(config, options))
+    .then(config => deepAssign(config, options))
     .then(replaceConfigSlashes)
     .then(normalizeConfig)
     .then(fromWorker ? noop : addPackageManagers)

lib/deppack/packages.js
@@ -1,8 +1,8 @@
 const sysPath = require('path');
+const deepAssign = require('deep-assign');
 const moduleNaming = require('./module-naming');
 const shims = require('./shims');
 const isRelative = require('./helpers').isRelative;
-const deepExtend = require('../helpers').deepExtend;
 const getModuleRootPath = require('./module-naming').getModuleRootPath;
 
 const canCallDep = (getPackageJson, path) => {
@@ -24,11 +24,7 @@ const canCallDep = (getPackageJson, path) => {
 
 const applyPackageOverrides = (pkg, rootPackage) => {
   const pkgOverride = rootPackage.overrides[pkg.name];
-
-  if (pkgOverride) {
-    pkg = deepExtend(pkg, pkgOverride);
-  }
-
+  if (pkgOverride) pkg = deepAssign({}, pkg, pkgOverride);
   return pkg;
 };
 

lib/fs_utils/pipeline.js
@@ -4,7 +4,7 @@ const debug = require('debug')('brunch:pipeline');
 const promisify = require('micro-promisify');
 const cachedRead = promisify(require('fcache').readFile);
 const promisifyPlugin = require('../helpers').promisifyPlugin;
-const deppack = require('../deppack');
+const deppack = require('../deppack'); // wrapInModule, exploreDeps, isNpm
 const processJob = require('../workers');
 
 // Pipeline: File -> File.

lib/fs_utils/source_file.js
@@ -1,11 +1,10 @@
 'use strict';
 const debug = require('debug')('brunch:file');
 const smap = require('source-map');
-const fcache = require('fcache');
-const promisify = require('micro-promisify');
+const readFile = require('micro-promisify')(require('fcache').readFile);
 
 const isNpmJSON = require('../deppack').isNpmJSON;
-const helpers = require('../helpers');
+const helpers = require('../helpers'); // below
 
 const pipeline = require('./pipeline').pipeline;
 
@@ -13,7 +12,6 @@ const prettify = helpers.prettify;
 const identityNode = helpers.identityNode;
 const replaceBackSlashes = helpers.replaceBackSlashes;
 const isWindows = helpers.isWindows;
-const readFile = promisify(fcache.readFile);
 
 const SourceMapConsumer = smap.SourceMapConsumer;
 const SourceNode = smap.SourceNode;

lib/helpers.js
@@ -3,7 +3,6 @@ const getBasename = require('path').basename;
 const isWindows = require('os').platform() === 'win32';
 const SourceNode = require('source-map').SourceNode;
 const promisify = require('micro-promisify');
-const slice = [].slice;
 
 exports.isWindows = isWindows;
 
@@ -17,30 +16,12 @@ const windowsStringReplace = (search, replacement) => {
   };
 };
 
-const objType = '[object Object]';
-const deepExtend = (object, properties, allObjects) => {
-  if (allObjects == null) allObjects = {};
-  const nestedObjs = Object.keys(allObjects).map(file => allObjects[file]);
-  Object.keys(properties).forEach(key => {
-    const value = properties[key];
-    if (toString.call(value) === objType && nestedObjs.indexOf(object) === -1) {
-      if (object[key] == null) object[key] = {};
-      deepExtend(object[key], value, allObjects);
-    } else {
-      object[key] = value;
-    }
-  });
-  return object;
-};
-
 // Single-level flatten.
 exports.flatten = (array) => [].concat.apply([], array);
 
 exports.promisifyPlugin = (len, f) => f.length === len ? f : promisify(f);
 exports.replaceSlashes = windowsStringReplace(/\//g, '\\');
 exports.replaceBackSlashes = windowsStringReplace(/\\/g, '\/');
-
-exports.deepExtend = deepExtend;
 
 exports.prettify = (object) => {
   return Object.keys(object).map(key => `${key}=${object[key]}`).join(' ');

commit 3f29c7b730962458b9637e43e1d1581007d1f61e

Fix a regression introduces by 48cb5e0. Closes GH-1239
The old and documented behavior is that overrides of files[type].key
should completely overwrite the said part of the main config, not be
merged with it.

lib/config.js
@@ -22,12 +22,14 @@ const mediator = {};
 const defaultConfigFilename = 'brunch-config';
 const defaultServerFilename = 'brunch-server';
 
-const customDeepAssign = (object, properties) => {
+const customDeepAssign = (object, properties, files) => {
+  const nestedObjs = Object.keys(files).map(file => files[file]);
+  const dontMerge = nestedObjs.indexOf(object) !== -1;
   Object.keys(properties).forEach(key => {
     const value = properties[key];
-    if (toString.call(value) === '[object Object]') {
+    if (toString.call(value) === '[object Object]' && !dontMerge) {
       if (object[key] == null) object[key] = {};
-      customDeepAssign(object[key], value);
+      customDeepAssign(object[key], value, files);
     } else {
       object[key] = value;
     }
@@ -74,7 +76,7 @@ const applyOverrides = (config, options) => {
         }));
       }
     });
-    customDeepAssign(config, overrideProps);
+    customDeepAssign(config, overrideProps, config.files);
   });
   return config;
 };