df88f11f4985fa3f94676e825b3e348fcebb6bfd
Paul Miller <paul@paulmillr.com>
2016-04-06 17:53:02 +0300

    Less code.

--- test/fs_utils.common.js
+++ test/fs_utils.common.js
@@ -4,5 +4,5 @@ const common = require('../lib/fs_utils/common');
 test('common: ignored: should ignore invalid files', (t) => {
   const files = ['app/assets/index.html', 'app/assets/favicon.ico', 'app/assets/.htaccess', 'app/assets/.rewrite', 'app/assets/#index.html#', 'app/assets/.index.html.swp'];
   const expectedIgnoredFiles = ['app/assets/#index.html#', 'app/assets/.index.html.swp'];
-  t.same(files.filter(common.ignored), expectedIgnoredFiles);
+  t.same(files.filter(common.isIgnored), expectedIgnoredFiles);
 });
