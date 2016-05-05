6601e84f3a5776b9534e520d1c014c4f3600a0b7
Artem Denysov <denysov.artem@gmail.com>
2016-05-05 14:27:23 +0300

    improve specs, fix eslint for ava async

--- test/config.js
+++ test/config.js
@@ -2,30 +2,32 @@ const path = require('path');
 const test = require('ava');
 const config = require('../lib/config');
 
-test('loads the config without overriding', t => {
+const getFolderName = function(path) {
+  return path.match(/([^\/]*)\/*$/)[1];
+};
+
+test('loads the config without overriding', async t => {
   const opts = {
     config: path.relative(__dirname, './fixtures/config-with-overrides.js')
   };
 
-  return config.loadConfig(false, opts, true).then((config) => {
-    const publicPath = config.paths.public.match(/([^\/]*)\/*$/)[1];
-    const appPath = config.paths.watched[0].match(/([^\/]*)\/*$/)[1];
-    const testPath = config.paths.watched[1].match(/([^\/]*)\/*$/)[1];
-    t.is(publicPath, 'public');
-    t.deepEqual([appPath, testPath], ['app', 'test']);
-  });
+  const brunchConfig = await config.loadConfig(false, opts, true);
+  brunchConfig.paths.public = getFolderName(brunchConfig.paths.public);
+  brunchConfig.paths.watched[0] = getFolderName(brunchConfig.paths.watched[0]);
+  brunchConfig.paths.watched[1] = getFolderName(brunchConfig.paths.watched[1]);
+  t.is(brunchConfig.paths.public, 'public');
+  t.deepEqual(brunchConfig.paths.watched, ['app', 'test']);
 });
 
-test('overrides the config using the specified env', t => {
+test('overrides the config using the specified env', async t => {
   const opts = {
     env: 'test',
     config: path.relative(__dirname, './fixtures/config-with-overrides.js')
   };
 
-  return config.loadConfig(false, opts, true).then((config) => {
-    const appPath = config.paths.watched[0].match(/([^\/]*)\/*$/)[1];
-    const testPath = config.paths.watched[1].match(/([^\/]*)\/*$/)[1];
-    t.is(config.paths.public, 'tmp');
-    t.deepEqual([appPath, testPath], ['app', 'test']);
-  });
+  const brunchConfig = await config.loadConfig(false, opts, true);
+  brunchConfig.paths.watched[0] = getFolderName(brunchConfig.paths.watched[0]);
+  brunchConfig.paths.watched[1] = getFolderName(brunchConfig.paths.watched[1]);
+  t.is(brunchConfig.paths.public, 'tmp');
+  t.deepEqual(brunchConfig.paths.watched, ['app', 'test']);
 });
