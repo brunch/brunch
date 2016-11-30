e67fdb97173d1f0f80d84d9d7ab3d64ee96af7bf
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-11-30 14:53:39 +0200

    adjust tests

--- test/fs_utils.is_ignored.js
+++ test/fs_utils.is_ignored.js
@@ -1,9 +1,9 @@
 'use strict';
 const test = require('ava-no-babel');
-const common = require('../lib/fs_utils/common');
+const isIgnored = require('../lib/fs_utils/is_ignored');
 
-test('common: ignored: should ignore invalid files', t => {
+test('isIgnored: ignored: should ignore invalid files', t => {
   const files = ['app/assets/index.html', 'app/assets/favicon.ico', 'app/assets/.htaccess', 'app/assets/.rewrite', 'app/assets/#index.html#', 'app/assets/.index.html.swp'];
   const expectedIgnoredFiles = ['app/assets/#index.html#', 'app/assets/.index.html.swp'];
-  t.deepEqual(files.filter(common.isIgnored), expectedIgnoredFiles);
+  t.deepEqual(files.filter(isIgnored), expectedIgnoredFiles);
 });
