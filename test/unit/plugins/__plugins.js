aced04dd0a73bbfcd0a808ccace18fd974e81785
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-08-28 01:21:59 +0300

    update plugins harness

--- test/unit/plugins/__plugins.js
+++ test/unit/plugins/__plugins.js
@@ -1,85 +1,44 @@
 'use strict';
 const Module = require('module');
-const origLoad = Module._load;
-
+const fs = require('fs');
 const sysPath = require('universal-path');
+
 const libPath = sysPath.resolve('lib');
 const configPath = require.resolve(`${libPath}/config`);
 const pluginsPath = require.resolve(`${libPath}/plugins`);
 
-const cwd = process.cwd();
-const os = require('os');
-const fs = require('fs');
-const cp = require('child_process');
+module.exports = (params = {}) => {
+  const {
+    modulesDir = 'node_modules',
+    modules = {},
+    config,
+  } = params;
 
-const testDir = `${os.tmpdir()}/brunch-plugins-test`;
-const npmDir = `${testDir}/node_modules`;
-
-const mkdir = dir => {
-  try {
-    fs.mkdirSync(dir);
-  } catch (err) {
-    if (err.code !== 'EEXIST') throw err;
-  }
-};
-
-before(() => {
-  mkdir(testDir);
-  process.chdir(testDir);
-});
-
-beforeEach(() => {
-  mkdir(npmDir);
-});
-
-afterEach(() => {
   delete require.cache[configPath];
   delete require.cache[pluginsPath];
 
-  Module._load = origLoad;
-  cp.execSync(`rm -rf ${npmDir}`);
-});
-
-after(() => {
-  process.chdir(cwd);
-  cp.execSync(`rm -rf ${testDir}`);
-});
-
-const config = exports.config = partConfig => {
-  if (!require.cache[configPath]) {
-    require(configPath).init(partConfig);
-  }
-};
-
-const patch = fn => {
-  const old = Module._load;
+  const {_load} = Module;
+  const {readdirSync} = fs;
 
   Module._load = function(...args) {
-    return fn.apply(this, args) || old.apply(this, args);
+    const [name] = args;
+    return name in modules ?
+      modules[name] :
+      _load.apply(this, args);
   };
-};
 
-exports.plugins = plugins => {
-  Object.keys(plugins).forEach(name => {
-    mkdir(`${npmDir}/${name}`);
-  });
+  fs.readdirSync = function(...args) {
+    const [dir] = args;
+    return dir === modulesDir ?
+      Object.keys(modules) :
+      readdirSync.apply(this, args);
+  };
 
-  patch(name => {
-    if (plugins.hasOwnProperty(name)) return plugins[name];
-  });
-};
-
-
-exports.trap = plugins => {
-  patch(name => {
-    if (plugins.includes(name)) {
-      throw new Error(`"${name}" should not have been required`);
-    }
-  });
-};
-
-exports.init = () => {
-  config();
-
-  return require(pluginsPath);
+  try {
+    require(configPath).init(config);
+    return require(pluginsPath);
+  } finally {
+    Module._load = _load;
+    fs.readdirSync = readdirSync;
+  }
 };
