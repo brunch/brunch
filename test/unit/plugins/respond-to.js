edfdd873fd037cefbeb2b567d3b63e72ed8741b1
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-08-30 21:47:02 +0300

    add `plugins.respondTo` test

---
+++ test/unit/plugins/respond-to.js
@@ -0,0 +1,145 @@
+'use strict';
+const init = require('./__plugins');
+
+describe('plugins.respondTo', () => {
+  it('is a function', () => {
+    init().respondTo.should.be.a('function');
+  });
+
+  it('returns plugins that have callable value by given name', () => {
+    const modules = {
+      'brunch-method': class {
+        get brunchPlugin() {
+          return true;
+        }
+        fn() {}
+      },
+      'brunch-gen-method': class {
+        get brunchPlugin() {
+          return true;
+        }
+        * fn() {}
+      },
+      'brunch-arrow': class {
+        constructor() {
+          this.fn = () => {};
+        }
+        get brunchPlugin() {
+          return true;
+        }
+      },
+      'brunch-function': class {
+        constructor() {
+          this.fn = function() {};
+        }
+        get brunchPlugin() {
+          return true;
+        }
+      },
+    };
+
+    init({modules})
+      .respondTo('fn')
+      .map(plugin => plugin.brunchPluginName)
+      .should.eql(Object.keys(modules));
+  });
+
+  it('rejects plugins that have truthy value by given name', () => {
+    init({
+      modules: {
+        'brunch-boolean': class {
+          constructor() {
+            this.fn = true;
+          }
+          get brunchPlugin() {
+            return true;
+          }
+        },
+        'brunch-number': class {
+          get brunchPlugin() {
+            return true;
+          }
+          get fn() {
+            return 21;
+          }
+        },
+        'brunch-string': class {
+          constructor() {
+            this.fn = 'fn';
+          }
+          get brunchPlugin() {
+            return true;
+          }
+        },
+        'brunch-symbol': class {
+          get brunchPlugin() {
+            return true;
+          }
+          get fn() {
+            return Symbol();
+          }
+        },
+        'brunch-object': class {
+          constructor() {
+            this.fn = {};
+          }
+          get brunchPlugin() {
+            return true;
+          }
+        },
+      },
+    }).respondTo('fn').should.be.empty;
+  });
+
+  it('rejects plugins that have falsy value by given name', () => {
+    init({
+      modules: {
+        'brunch-missing': class {
+          get brunchPlugin() {
+            return true;
+          }
+        },
+        'brunch-undefined': class {
+          constructor() {
+            this.fn = undefined;
+          }
+          get brunchPlugin() {
+            return true;
+          }
+        },
+        'brunch-null': class {
+          get brunchPlugin() {
+            return true;
+          }
+          get fn() {
+            return null;
+          }
+        },
+        'brunch-boolean': class {
+          constructor() {
+            this.fn = false;
+          }
+          get brunchPlugin() {
+            return true;
+          }
+        },
+        'brunch-number': class {
+          get brunchPlugin() {
+            return true;
+          }
+          get fn() {
+            return 0;
+          }
+        },
+        'brunch-string': class {
+          constructor() {
+            this.fn = '';
+          }
+          get brunchPlugin() {
+            return true;
+          }
+        },
+      },
+    }).respondTo('fn').should.be.empty;
+  });
+});
